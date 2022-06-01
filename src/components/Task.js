2fbc59102db8bb9dd527f371ec7a1a4a92cb55da
ElenaG518 <patty518@yahoo.com>
2022-06-01 14:13:29 -0700

    create Task component

---
+++ src/components/Task.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Task = ({
+        task: {label, completed},
+        onToggle
+    }) => (
+        <li className={completed ? 'completed' : null} onClick={onToggle}>
+            {label}
+        </li>
+    );
+    
+export default Task;
