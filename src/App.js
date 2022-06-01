aa140ef8d951e69f2b7ac729b7d17b06a77300c5
ElenaG518 <patty518@yahoo.com>
2022-06-01 15:51:33 -0700

    Add callback prop to TaskList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
+import './App.css';
 
 function App() {
 const [tasks, setTasks] = useState([]);
-const handleNewTask = (task) => setTasks([...tasks, task]);
+
+const handleNewTask = (task) => setTasks([...tasks, {completed: false, label: task}]);
+const handleToggleTask = (taskIdx) => {
+  const newTasks = [...tasks];
+  newTasks[taskIdx] = {...newTasks[taskIdx], completed: !newTasks[taskIdx].completed};
+  setTasks(newTasks);
+};
 
   return (
     <div>
       <TaskInput onSubmit={handleNewTask}/>
-      <TaskList tasks={tasks}/>
+      <TaskList tasks={tasks} onToggleTask={handleToggleTask}/>
     </div>
   );
 }
