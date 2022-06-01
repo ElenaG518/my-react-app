73e501e6faa1f1bc914a74a78cc3243a069a548e
ElenaG518 <patty518@yahoo.com>
2022-06-01 15:52:52 -0700

    Test toggle callback; modify task variable to an object

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,14 +1,26 @@
 import React from "react";
 
 import TaskList from "./TaskList";
-import {render, screen } from '@testing-library/react'
+import {fireEvent, render, screen } from '@testing-library/react'
 
 describe('<TaskList />', () => {
-    it('must render tasks', () => {
-        const tasks = ['Do this', 'Do that', 'Do nothing'];
-        render(<TaskList tasks={tasks} />); 
+    const tasks = [
+        {label: 'Do this', completed: false},
+        {label: 'Do that', completed: true},
+      ];
 
+    it('must render tasks', () => {
+        render(<TaskList tasks={tasks} />); 
         const renderedTasks = tasks.map(taks => screen.getByText('Do this'));
-        expect(renderedTasks.length).toBe(3);
-    })
+        expect(renderedTasks.length).toBe(2);
+    });
+
+    it('must call onToggleTask with correct task index', () => {
+          const mockOnToggle = jest.fn();
+          render(<TaskList tasks={tasks} onToggleTask={mockOnToggle} />);
+          const renderedTasks = tasks.map(task => screen.getByText(task.label));
+          fireEvent.click(renderedTasks[1]);
+          expect(mockOnToggle).toHaveBeenCalledWith(1);
+        
+    });
 })
