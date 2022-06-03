fa5463314f450e21ee94c375ff0c84a200052cfa
ElenaG518 <patty518@yahoo.com>
2022-06-03 14:14:42 -0700

    Implement hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
 import './App.css';
+import useTasks from './hooks/useTasks';
 
 function App() {
-const [tasks, setTasks] = useState([]);
+const [tasks, {createTask, toggleTask}] = useTasks();
 
-const handleNewTask = (task) => setTasks([...tasks, {completed: false, label: task}]);
-const handleToggleTask = (taskIdx) => {
-  const newTasks = [...tasks];
-  newTasks[taskIdx] = {...newTasks[taskIdx], completed: !newTasks[taskIdx].completed};
-  setTasks(newTasks);
-};
+// const handleNewTask = (task) => setTasks([...tasks, {completed: false, label: task}]);
+// const handleToggleTask = (taskIdx) => {
+//   const newTasks = [...tasks];
+//   newTasks[taskIdx] = {...newTasks[taskIdx], completed: !newTasks[taskIdx].completed};
+//   setTasks(newTasks);
+// };
 
   return (
     <div>
-      <TaskInput onSubmit={handleNewTask}/>
-      <TaskList tasks={tasks} onToggleTask={handleToggleTask}/>
+      <TaskInput onSubmit={createTask}/>
+      <TaskList tasks={tasks} onToggleTask={toggleTask}/>
     </div>
   );
 }
