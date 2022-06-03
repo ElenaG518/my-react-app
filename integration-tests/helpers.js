28a6475b6a34d7cb5939f471a75a45d210cdaef1
ElenaG518 <patty518@yahoo.com>
2022-06-03 14:02:52 -0700

    Add createTask method

--- integration-tests/helpers.js
+++ integration-tests/helpers.js
@@ -1,9 +1,19 @@
 import webdriver from 'selenium-webdriver';
 import chrome from 'chromedriver';
+import {until, By, Key} from 'selenium-webdriver';
 
 export const getDriver = () => {
   let driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
   return driver;
+};
+
+export const createTask = async (taskName, driver) => {
+  await driver.wait(until.elementLocated(By.xpath("//input[@placeholder='Enter new task']")), 1000);
+  const input = await driver.findElement(By.xpath("//input[@placeholder='Enter new task']"));
+  await input.clear();
+  await input.sendKeys(taskName + Key.ENTER);
+
+  await driver.wait(until.elementLocated(By.xpath(`//*[text()='${taskName}']`)), 1000);
 };
